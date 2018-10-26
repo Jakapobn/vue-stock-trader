88e1e500745735b7ac23b8550d5aff6cff545005
InWMentZa <jakapob@vertice-international.com>
2018-10-26 08:28:11 +0700

    set path

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-stock-trader',
     proxyTable: {},
 
     // Various Dev Server settings
